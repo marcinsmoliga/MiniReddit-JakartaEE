8af0054f36411cdc136ea0d5582f6fbdc6702961
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-19 20:12:30 +0100

    class DiscoveryDao/discovery created :: method findAll() created

---
+++ src/main/java/com/example/MiniReddit/domain/discovery/DiscoveryDao.java
@@ -0,0 +1,57 @@
+package com.example.MiniReddit.domain.discovery;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.example.MiniReddit.config.DataSourceProvider;
+
+public class DiscoveryDao {
+	private final DataSource dataSource;
+
+	public DiscoveryDao(DataSource dataSource) {
+		try {
+			this.dataSource = DataSourceProvider.getDataSource();
+		} catch (NamingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Discovery> findAll() {
+		final String query = """
+				SELECT
+					id, title, url description, date_added, category_id
+				FROM
+					discovery d
+				""";
+		try (Connection connection = dataSource.getConnection();
+		     Statement statement = connection.createStatement()) {
+			ResultSet resultSet = statement.executeQuery(query);
+			List<Discovery> allDiscoveries = new ArrayList<>();
+			while (resultSet.next()) {
+				Discovery discovery = mapRow(resultSet);
+				allDiscoveries.add(discovery);
+			}
+			return allDiscoveries;
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Discovery mapRow(ResultSet set) throws SQLException {
+		int discoveryId = set.getInt("id");
+		String title = set.getString("title");
+		String url = set.getString("url");
+		String description = set.getString("description");
+		LocalDateTime dateAdded = set.getTimestamp("date_added").toLocalDateTime();
+		int categoryId = set.getInt("category_id");
+		return new Discovery(discoveryId, title, url,description, dateAdded, categoryId);
+	}
+}
