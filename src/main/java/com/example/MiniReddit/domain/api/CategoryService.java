69d6bc0a9f844ca014008c6b5c9ed61ead95182a
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-20 15:07:07 +0100

    method findById(...)/CategoryService created

--- src/main/java/com/example/MiniReddit/domain/api/CategoryService.java
+++ src/main/java/com/example/MiniReddit/domain/api/CategoryService.java
@@ -1,6 +1,7 @@
 package com.example.MiniReddit.domain.api;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.example.MiniReddit.domain.category.Category;
@@ -16,11 +17,26 @@ public class CategoryService {
 				.collect(Collectors.toList());
 	}
 
+	public Optional<CategoryFullInfo> findById(int categoryId) {
+		return categoryDao.findById(categoryId)
+				.map(CategoryFullInfoMapper::map);
+	}
+
 	private static class CategoryNameMapper {
 		static CategoryName map(Category category) {
 			return new CategoryName(
 					category.getId(),
 					category.getName());
+		}
+	}
+
+	private static class CategoryFullInfoMapper {
+		static CategoryFullInfo map(Category category) {
+			return new CategoryFullInfo(
+					category.getId(),
+					category.getName(),
+					category.getDescription()
+			);
 		}
 	}
 }
