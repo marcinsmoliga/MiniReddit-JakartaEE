f0339417a0a760fcf6b8264c6ca7b0c87ae69fd3
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-20 19:02:15 +0100

    CategoryController/category created :: method doGet(...) overriden

---
+++ src/main/java/com/example/MiniReddit/client/category/CategoryController.java
@@ -0,0 +1,34 @@
+package com.example.MiniReddit.client.category;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.example.MiniReddit.domain.api.CategoryFullInfo;
+import com.example.MiniReddit.domain.api.CategoryService;
+import com.example.MiniReddit.domain.api.DiscoveryBasicInfo;
+import com.example.MiniReddit.domain.api.DiscoveryService;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/category")
+public class CategoryController extends HttpServlet {
+	private final CategoryService categoryService = new CategoryService();
+	private final DiscoveryService discoveryService = new DiscoveryService();
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int categoryId = Integer.parseInt(request.getParameter("id"));
+
+		CategoryFullInfo category = categoryService.findById(categoryId).orElseThrow();
+		request.setAttribute("category", category);
+
+		List<DiscoveryBasicInfo> discoveries = discoveryService.findByCategory(categoryId);
+		request.setAttribute("discoveries", discoveries);
+
+		request.getRequestDispatcher("/category.jsp").forward(request, response);
+	}
+}
