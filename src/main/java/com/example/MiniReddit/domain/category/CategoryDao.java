a937a1487a6cbe25685889b11372dcad9de7577d
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-20 15:01:26 +0100

    method findById(...)/CategoryDao created

--- src/main/java/com/example/MiniReddit/domain/category/CategoryDao.java
+++ src/main/java/com/example/MiniReddit/domain/category/CategoryDao.java
@@ -2,11 +2,13 @@ package com.example.MiniReddit.domain.category;
 
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -46,6 +48,34 @@ public class CategoryDao {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	public Optional<Category> findById(int categoryId) {
+		final String query = """
+					SELECT
+						id, name, description
+					FROM
+						category
+					WHERE 
+						id = ?
+					""";
+		try (
+				Connection connection = dataSource.getConnection();
+				PreparedStatement statement = connection.prepareStatement(query)
+		) {
+			statement.setInt(1, categoryId);
+			ResultSet resultSet = statement.executeQuery(query);
+
+			if (resultSet.next()) {
+				Category category = mapRow(resultSet);
+				return Optional.of(category);
+			} else {
+				return Optional.empty();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+
 	}
 
 	private static Category mapRow(ResultSet resultSet) throws SQLException {
