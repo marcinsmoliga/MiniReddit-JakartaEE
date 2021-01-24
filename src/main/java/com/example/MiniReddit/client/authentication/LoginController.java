3d183fc5cf0fe0091b7e48e3cab0aeff6b75ae90
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-24 22:17:04 +0100

    class LoginController created :: method doGet() overridden

---
+++ src/main/java/com/example/MiniReddit/client/authentication/LoginController.java
@@ -0,0 +1,22 @@
+package com.example.MiniReddit.client.authentication;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.HttpMethodConstraint;
+import jakarta.servlet.annotation.ServletSecurity;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+@ServletSecurity(httpMethodConstraints = {
+		@HttpMethodConstraint(value = "GET", rolesAllowed = "USER")
+})
+public class LoginController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.sendRedirect(request.getContextPath());
+	}
+}
