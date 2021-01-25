7396b214958ad6d77bbdcce9b35256be51316208
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-25 19:16:00 +0100

    class Discovery and DiscoveryDao updated

--- src/main/java/com/example/MiniReddit/domain/discovery/DiscoveryDao.java
+++ src/main/java/com/example/MiniReddit/domain/discovery/DiscoveryDao.java
@@ -19,11 +19,11 @@ public class DiscoveryDao extends BaseDao {
 
 	public List<Discovery> findAll() {
 		final String query = """
-				SELECT
-					id, title, url, description, date_added, category_id
-				FROM
-					discovery d
-				""";
+                SELECT
+                    id, title, url, description, date_added, category_id, user_id
+                FROM
+                    discovery d
+                """;
 		try (Connection connection = getConnection();
 		     Statement statement = connection.createStatement()) {
 			ResultSet resultSet = statement.executeQuery(query);
@@ -40,23 +40,19 @@ public class DiscoveryDao extends BaseDao {
 
 	public List<Discovery> findByCategory(int categoryId) {
 		final String query = """
-				SELECT
-					id, title, url, description, date_added, category_id
-				FROM
-					discovery
-				WHERE
-					category_id = ?
-				""";
-
-		try (
-				Connection connection = getConnection();
-				PreparedStatement statement = connection.prepareStatement(query)
-		) {
+                SELECT
+                    id, title, url, description, date_added, category_id, user_id
+                FROM
+                    discovery
+                WHERE
+                    category_id = ?
+                """;
+		try (Connection connection = getConnection();
+		     PreparedStatement statement = connection.prepareStatement(query)) {
 			statement.setInt(1, categoryId);
 			ResultSet resultSet = statement.executeQuery();
-
 			List<Discovery> discoveries = new ArrayList<>();
-			while(resultSet.next()) {
+			while (resultSet.next()) {
 				Discovery discovery = mapRow(resultSet);
 				discoveries.add(discovery);
 			}
@@ -64,16 +60,41 @@ public class DiscoveryDao extends BaseDao {
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		}
-
 	}
 
-	private static Discovery mapRow(ResultSet resultSett) throws SQLException {
-		int discoveryId = resultSett.getInt("id");
-		String title = resultSett.getString("title");
-		String url = resultSett.getString("url");
-		String description = resultSett.getString("description");
-		LocalDateTime dateAdded = resultSett.getTimestamp("date_added").toLocalDateTime();
-		int categoryId = resultSett.getInt("category_id");
-		return new Discovery(discoveryId, title, url,description, dateAdded, categoryId);
+	public void save(Discovery discovery) {
+		final String query = """
+                        INSERT INTO
+                            discovery (title, url, description, date_added, category_id, user_id)
+                        VALUES
+                            (?, ?, ?, ?, ?, ?)
+                        """;
+		try (Connection connection = getConnection();
+		     PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+			statement.setString(1, discovery.getTitle());
+			statement.setString(2, discovery.getUrl());
+			statement.setString(3, discovery.getDescription());
+			statement.setObject(4, discovery.getDateAdded());
+			statement.setInt(5, discovery.getCategoryId());
+			statement.setInt(6, discovery.getUserId());
+			statement.executeUpdate();
+			ResultSet generatedKeys = statement.getGeneratedKeys();
+			if (generatedKeys.next()) {
+				discovery.setId(generatedKeys.getInt(1));
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Discovery mapRow(ResultSet set) throws SQLException {
+		int discoveryId = set.getInt("id");
+		String title = set.getString("title");
+		String url = set.getString("url");
+		String description = set.getString("description");
+		LocalDateTime dateAdded = set.getTimestamp("date_added").toLocalDateTime();
+		int categoryId = set.getInt("category_id");
+		int userId = set.getInt("user_id");
+		return new Discovery(discoveryId, title, url, description, dateAdded, categoryId, userId);
 	}
 }
