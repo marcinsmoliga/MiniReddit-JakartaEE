cf9876439bd68a4e17afca858844d0f25a5ce36d
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-25 18:32:41 +0100

    method hashPasswordWithSha256()/UserService created

--- src/main/java/com/example/MiniReddit/domain/api/UserService.java
+++ src/main/java/com/example/MiniReddit/domain/api/UserService.java
@@ -1,6 +1,9 @@
 package com.example.MiniReddit.domain.api;
 
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDateTime;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 import com.example.MiniReddit.domain.user.User;
 import com.example.MiniReddit.domain.user.UserDao;
@@ -10,7 +13,17 @@ public class UserService {
 
 	public void register(UserRegistration userRegistration) {
 		User userToSave = UserMapper.map(userRegistration);
-		userDao.save(userToSave);
+		try {
+			hashPasswordWithSha256(userToSave);
+			userDao.save(userToSave);
+		} catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private void hashPasswordWithSha256(User user) throws NoSuchAlgorithmException{
+		String sha256Password = DigestUtils.sha3_256Hex(user.getPassword());
+		user.setPassword(sha256Password);
 	}
 
 	private static class UserMapper {
--- src/main/java/com/example/MiniReddit/domain/user/User.java
+++ src/main/java/com/example/MiniReddit/domain/user/User.java
@@ -32,6 +32,10 @@ public class User {
 		return password;
 	}
 
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public LocalDateTime getRegistrationDate() {
 		return registrationDate;
 	}
