eb9d90d41817c32477de47b7bd1d0a1693f0c08a
marcinsmoliga <50251884+marcinsmoliga@users.noreply.github.com>
2021-01-26 19:43:47 +0100

    method findById()/UserDao created

--- src/main/java/com/example/MiniReddit/domain/user/UserDao.java
+++ src/main/java/com/example/MiniReddit/domain/user/UserDao.java
@@ -77,6 +77,28 @@ public class UserDao extends BaseDao {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public Optional<User> findById(int id) {
+		final String query = """
+                SELECT
+                    id, username, email, password, registration_date
+                FROM
+                    user
+                WHERE
+                    id = ?
+                """;
+		try (Connection connection = getConnection();
+		     PreparedStatement statement = connection.prepareStatement(query)) {
+			statement.setInt(1, id);
+			ResultSet resultSet = statement.executeQuery();
+			if (resultSet.next())
+				return Optional.of(mapRow(resultSet));
+			else
+				return Optional.empty();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	private User mapRow(ResultSet resultSet) throws SQLException {
 		int id = resultSet.getInt("id");
